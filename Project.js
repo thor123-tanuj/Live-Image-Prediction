acfad3f3dce73dd5eb7c3ace0e7681180ec9ce2a
Tanuj.P <67108309+thor123-tanuj@users.noreply.github.com>
2021-09-02 17:28:57 +0530

    Add files via upload

---
+++ Project.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import PickImageAlphabet from './ImageDeduction/screens/alphabet'
+
+export default class App extends React.Component {
+render() {
+  return(
+    <PickImageAlphabet/>
+  )
+}
+}
